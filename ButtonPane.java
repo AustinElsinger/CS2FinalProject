9761d87a687f186b5d97c04c905bedb6592ae661
AustinElsinger <49951411+AustinElsinger@users.noreply.github.com>
2019-04-26 23:11:39 -0500

    Create ButtonPane.java

---
+++ ButtonPane.java
@@ -0,0 +1,43 @@
+package application;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+public class ButtonPane extends GridPane
+{	
+	private Button[] alphabet;
+	
+	public ButtonPane()
+	{
+		alphabet = new Button[26];
+		
+		char letter = 'A';
+		for (int i = 0; i < 26; i++)
+		{
+			alphabet[i] = new Button("" + letter);
+			alphabet[i].setMinSize(30, 30);
+			letter++;
+		}
+		
+		for (int i = 0; i < 26; i++)
+		{
+			if (i < 10)
+			{
+				add(alphabet[i], i, 0);
+			}
+			else if (i > 9 && i < 20)
+			{
+				add(alphabet[i], i - 10, 1);
+			}
+			else
+			{
+				add(alphabet[i], i - 18, 2);
+			}
+		}
+		
+		setAlignment(Pos.CENTER);
+		setPrefHeight(150);
+		setPrefWidth(400);
+	}
+}
